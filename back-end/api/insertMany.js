1e07f0ca78d49bbcbb8e6457f6eed53ab8582df1
Hugobertoncelo <hugobertoncelo@gmail.com>
2025-02-14 08:11:36 -0300

    new songs

--- back-end/api/insertMany.js
+++ back-end/api/insertMany.js
@@ -1,23 +1,25 @@
-import { artistArray } from "../../front-end/assets/database/artists";
-import { songsArray } from "../../front-end/assets/database/songs";
+import { artistArray } from "../../front-end/src/assets/database/artists.js";
+import { songsArray } from "../../front-end/src/assets/database/songs.js";
 import { db } from "./connect.js";
 
 const newArtistArray = artistArray.map((currentArtistObj) => {
   const newArtistObj = { ...currentArtistObj };
-  delete newArtistObj.id
+  delete newArtistObj.id;
 
   return newArtistObj;
 });
 
 const newSongsArray = songsArray.map((currentSongObj) => {
   const newSongObj = { ...currentSongObj };
-  delete newSongObj.id
-  
+  delete newSongObj.id;
+
   return newSongObj;
 });
 
-const resSongs = await db.collection("songs").insertMany(newSongsArray);
-const resArtists = await db.collection("artists").insertMany(newArtistArray);
+const responseSongs = await db.collection("songs").insertMany(newSongsArray);
+const responseArtists = await db
+  .collection("artists")
+  .insertMany(newArtistArray);
 
-console.log(newArtistArray);
-console.log(newSongsArray);
+console.log(responseSongs);
+console.log(responseArtists);
--- back-end/api/server.js
+++ back-end/api/server.js
@@ -1,3 +1,9 @@
+// API significa Application Programming Interface
+// POST, GET, PUT, DELETE
+// CRUD - Create Read Update Delete
+// Endpoint
+// Middleware
+
 import express from "express";
 import cors from "cors";
 import { db } from "./connect.js";
@@ -5,20 +11,20 @@ import { db } from "./connect.js";
 const app = express();
 const PORT = 3000;
 
-// app.use(cors());
+app.use(cors());
 // app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.send("artists e songs")
-})
+app.get("/", (request, response) => {
+  response.send("Só vamos trabalhar com os endpoints '/artists' e '/songs'");
+});
 
-app.get('/artists', async (req, res) => {
-  res.send(await db.collection("artists").find({}).toArray());
-})
+app.get("/artists", async (request, response) => {
+  response.send(await db.collection("artists").find({}).toArray());
+});
 
-app.get('/songs', async (req, res) => {
-  res.send(await db.collection("songs").find({}).toArray());
-})
+app.get("/songs", async (request, response) => {
+  response.send(await db.collection("songs").find({}).toArray());
+});
 
 app.listen(PORT, () => {
   console.log(`Servidor está escutando na porta ${PORT}`);
