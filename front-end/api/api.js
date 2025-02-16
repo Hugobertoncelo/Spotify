85ea3d58f848f001d8124c5f69e953bdf4b53312
Hugobertoncelo <hugobertoncelo@gmail.com>
2025-02-16 20:44:48 -0300

    ajusts deploy

--- front-end/api/api.js
+++ front-end/api/api.js
@@ -2,7 +2,8 @@ import axios from "axios";
 // import "dotenv/config"
 
 // const { NODE_ENV } = process.env;
-const URL =  import.meta.env.DEV ? "http://localhost:3000/api" : "/api";
+// const URL =  import.meta.env.DEV ? "http://localhost:3000/api" : "/api";
+const URL = "https://spotify-df33.onrender.com/api";
 
 const responseArtists = await axios.get(`${URL}/artists`);
 const responseSongs = await axios.get(`${URL}/songs`);
